cba88711b93e053ef7b47cc5820ea635cc3c1973
PrimeRP8008 <rohitpanghal3031@gmail.com>
2024-11-25 06:30:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,25 +3,25 @@ import os
 
 
 # Required Variables Config
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
+API_ID = int(os.environ.get("API_ID", "28421635"))
+API_HASH = os.environ.get("API_HASH", "a4856de5fec0b9b3601ff06425f3f58e")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7914025859:AAH1N2D3Om1KzlCc1lWXMRAwb52NMCTdFMM")
+ADMIN = int(os.environ.get("ADMIN", "1534632634"))
 
 
 # Premium 4GB Renaming Client Config
-STRING_SESSION = os.environ.get("STRING_SESSION", "")
+STRING_SESSION = os.environ.get("STRING_SESSION", "BQGxrgMAtsbULY6sQD8RsVo-fWPicfo7fYGGB9C4pwiYFDr3AAIC3zBKEo3D5YybZtijKQVveAEbdsTK5nJm-WjXuO9soX3wM1ruwAWeh6xe8E6sbt9tScIDkAUSpQre71Ac6rQBBcIB_AgAvA7fryDZ7Jh3a7x9CQtHlEFgJ-Ky-4yjgXPb0hiRwkL6sXjIpfURCSbFplEMxpGd4H6U2Y0Ypj3FEn_UzlANUKGJNVtZ45gC_QWAK8lelzrMa1p-LRWqqEvyZWzgUzecKLGlzUczd5q40fCZjJUB6FsnwPlwdeCYWAoLbzq9wuE7GT3K-FxEVZ3l6-iPqxlz0G98NmBwEkvphAAAAABbeKK6AA")
 
 
 # Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "-1001991806323")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002093375923"))
 
 
 # Mongo DB Database Config
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "CodeXBots")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://rohitl3031:CNHkZh4Nkgv0mUuJ@cluster0.cgmci.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 
 # Other Variables Config
-START_PIC = os.environ.get("START_PIC", "https://graph.org/file/ada3f739fed7efdbe7b08.jpg")
+START_PIC = os.environ.get("START_PIC", "https://graph.orgfile:/data/user/0/com.telex/cache/1000057389")
